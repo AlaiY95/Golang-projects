2ff2b00b1508e6b413ba8006d86fb681673e2bdf
AlaiY95 <37549558+AlaiY95@users.noreply.github.com>
2023-04-02 15:32:22 +0100

    comments amended

--- API2-Mongodb/main.go
+++ API2-Mongodb/main.go
@@ -1,17 +1,3 @@
-// The code is a basic implementation of a RESTful API in Go, using the httprouter package for routing and the mgo package for connecting to a MongoDB database. Here's a breakdown of the code:
-
-// The code imports the necessary packages: httprouter, mgo, and net/http. The controllers package is also imported, which will contain the handlers for the various HTTP routes.
-
-// The main function sets up a new httprouter instance and creates a new UserController, passing in the result of the getSession function. The UserController is used to handle requests to the various routes.
-
-// The main function then sets up the routes using the httprouter instance's GET, POST, and DELETE methods. Each route corresponds to a method on the UserController.
-
-// Finally, the main function starts an HTTP server listening on port 9000 of the local machine.
-
-// The getSession function returns a new mgo session connected to a MongoDB database running on the default port (27017) of the local machine. If there is an error connecting to the database, the function panics.
-
-// Overall, this code demonstrates a simple implementation of a RESTful API in Go, with MongoDB as the underlying database. The code is relatively concise and easy to follow, although it could be improved with additional error handling and more robust database connections.
-
 package main
 
 import (
